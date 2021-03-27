fbb91dae492af716e5211a5e4802010522a99839
Soyeon Park <paksuua@naver.com>
2021-03-27 09:44:36 +0900

    ADD 완주하지 못한 선수

--- Hash/완주하지 못한 선수/soyeon.py
+++ Hash/완주하지 못한 선수/soyeon.py
@@ -8,14 +8,22 @@ def solution(participant, completion):
         
     return participant[len(completion)]
 
-# 효율성 테스트 실패
+# collections 함수 사용
+# import collections
 # def solution(participant, completion):
-#     participant.sort()
-#     completion.sort()
-    
-#     for p_name in participant:
-#         if p_name not in completion:
-#             return p_name
-            
-#         else:
-#             del(completion[completion.index(p_name)])
+#     answer = collections.Counter(participant) - collections.Counter(completion)
+#     return list(answer.keys())[0]
+
+
+# def solution(participant, completion):
+#     answer = ''
+#     temp = 0
+#     dic = {}
+#     for part in participant:
+#         dic[hash(part)] = part
+#         temp += int(hash(part))
+#     for com in completion:
+#         temp -= hash(com)
+#     answer = dic[temp]
+
+#     return answer
